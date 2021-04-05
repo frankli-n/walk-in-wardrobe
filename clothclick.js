2285de09ebb695f81b8e424d0e3e5ea8d0356568
theFrank <56799408+frankli-n@users.noreply.github.com>
2021-04-05 14:39:42 -0700

    getting stuff from DWOP

---
+++ clothclick.js
@@ -0,0 +1,10 @@
+function showAtticus(){
+	document.getElementById("atticushead").style.visibility = "visible";
+}
+
+function showFrankie(){
+	document.getElementById("frankiehead").style.visibility = "visible";
+}
+function showLuke(){
+	document.getElementById("lukehead").style.visibility = "visible";
+}
